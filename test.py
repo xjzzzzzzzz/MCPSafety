49868a05a0dff2064b2773dcdadb4776bb875eab
zongxuanjun <1710166435@qq.com>
2025-08-26 23:30:04 +0800

    YFinance

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-def alert_user(msg):
-    # Directly injecting user input into a shell command
-    os.system(f"echo {msg}")
-if __name__ == "__main__":
-    alert_user("MSFT price is above $300;echo $PATH")
